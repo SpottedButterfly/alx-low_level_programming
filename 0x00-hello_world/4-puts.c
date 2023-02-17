d15c9f3bce839d79fd9ecdb92871946d7965dfdc
Sinakweyinkosi <dlaminisinakweyinkosi@gmail.com>
2023-02-16 18:37:48 -0800

    scripts that assemble, compile, and a code that prints string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * a program that prints:
+ * "Programming is like building a multilingual
+ * puzzle" followed bu a new line
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
