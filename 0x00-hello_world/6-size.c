0ff2b03f33872f8a8c89d9ab611fa370246ca5ac
Sinakweyinkosi <dlaminisinakweyinkosi@gmail.com>
2023-02-17 06:22:41 -0800

    hot fix on c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	printf("Size of a char: %c", sizeof(char), " byte(s)");
 	printf("Size of an int: %i", sizeof(int), " byte(s)");
-	printf("Size of a long int: %ld", sizeof(long), " byte(s)");
+	printf("Size of a long int: %ld", sizeof(long int), " byte(s)");
 	printf("Size of a long long int: %lld", sizeof(long long int), " byte(s)");
 	printf("Size of a float: %f", sizeof(float), " byte(s)");
 	Return (0);
