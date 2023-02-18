9d5c1b79f305ca54f394213ef1836efb3f180228
Sinakweyinkosi <dlaminisinakweyinkosi@gmail.com>
2023-02-17 20:38:03 -0800

    C code that assigns a random number to a variable then checks whether the number is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - assigning a random number to variable n
+ * and printing whether the number stored in the variable
+ * is positive or negative.
+ * @n: variable to be checked
+ *
+ * Return: 0 if successful. 1 otherwise
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive", n);
+	else if (n < 0)
+		printf("%i is negative", n);
+	else (n == 0)
+		printf("%i is zero", n)
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This directory has scripts and C code based on variables,if,elseand while
