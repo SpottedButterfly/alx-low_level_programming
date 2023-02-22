b493a3d03b88b44c07c1f5b4cfe23f62fe0ab006
Sinakweyinkosi <dlaminisinakweyinkosi@gmail.com>
2023-02-22 03:46:58 -0800

    0-putchar c code fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * main - prints _putchar
@@ -7,6 +8,20 @@
  */
 int main(void)
 {
-	_putchar(char c);
+	char c[9] = "_putchar\n";
+
+	_putchar(c[9]);
 	return (0);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
