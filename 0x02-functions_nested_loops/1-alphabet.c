a53cb4f5522856cd85511ad6a193bc8e17c70b6a
Sinakweyinkosi <dlaminisinakweyinkosi@gmail.com>
2023-03-03 01:56:09 -0800

    printing the alphabet 10 times

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,19 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
  *
  * Return: 0 if successful
  */
 void print_alphabet(void);
 
-int main(void)
-{
-	print_alphabet();
-	return (0);	
-}
+/**
+* int main(void)
+* {
+*	print_alphabet();
+*	return (0);	
+* }
+*/
 
 void print_alphabet(void)
 {
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet
+ * 10 times in lowercase.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char letters;
+	char n = '\n';
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+		_putchar(n);
+	}
+
+	return;
+}
