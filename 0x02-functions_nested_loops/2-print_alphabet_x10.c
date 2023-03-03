66d6939a92ce474f99771c526ef18479f3941271
Sinakweyinkosi <dlaminisinakweyinkosi@gmail.com>
2023-03-03 02:17:08 -0800

    islower - a function that checks for lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,6 +12,7 @@ void print_alphabet_x10(void)
 {
 	char letters;
 	char n = '\n';
+	int i;
 
 	for (i = 0; i <= 10; i++)
 	{
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - checks for lowercase
+ *
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _islower(int c);
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
