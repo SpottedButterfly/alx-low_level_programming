08da01447d61912b5c0462fec66f8d7b1f5f57e4
Sinakweyinkosi Dlamini <dlaminisinakweyinkosi@gmail.com>
2023-04-28 01:29:31 -0700

    Task 3:  3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+
+	_putchar('\n');
+	return;
+}
