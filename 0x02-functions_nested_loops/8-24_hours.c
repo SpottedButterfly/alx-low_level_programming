5be49f07501fa58a5b4fdaf3ef72abc1a866a947
Sinakweyinkosi Dlamini <dlaminisinakweyinkosi@gmail.com>
2023-05-04 07:02:48 -0700

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (minutes / 10));
+			_putchar('0' + (minutes % 10));
+			_putchar('\n');
+		}
+	}
+}
